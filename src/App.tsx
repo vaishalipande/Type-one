dbfb1a462057201fe29d7f10a758b5ce6216f6b6
Va18041991 <vaishali0491@gmail.com>
2023-05-04 23:07:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+const App() => {
+let title: string = 'from typescript 123';
+
+
+
+  return (
+    <div>
+     {title}
+    </div>
+  );
+}
+
+export default App;
